86b1f0b11b4951264d3fe50115611f6ab61abf76
gaurav <gbombatkar@gmail.com>
2020-05-11 20:10:34 +0530

    deploy web page

---
+++ build/precache-manifest.56a3f08166a6ffdc7573f3114e68cc57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5af10a87df1978844040966e4005ba",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "f0558f50c4b7030eca6f",
+    "url": "/covid-19/static/css/main.ba6cba90.chunk.css"
+  },
+  {
+    "revision": "44d26eaef38ae1292caa",
+    "url": "/covid-19/static/js/2.e9309652.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.e9309652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0558f50c4b7030eca6f",
+    "url": "/covid-19/static/js/main.7744d6a6.chunk.js"
+  },
+  {
+    "revision": "f6ecbbdf84e1f735035c",
+    "url": "/covid-19/static/js/runtime-main.1ae4e48e.js"
+  },
+  {
+    "revision": "7cac1347b3c46ec0cd2f991c7b1f14b6",
+    "url": "/covid-19/static/media/image.7cac1347.png"
+  }
+]);
